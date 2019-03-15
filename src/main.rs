1a77cb312eb174cc6cd57015b30914e053e9cf01
Sindre Johansen <sindre@iterate.no>
2019-03-15 10:24:35 +0100

    Refactoring

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,13 @@ use byteorder::{ByteOrder, LittleEndian};
 
 struct Machine {
     mem: Vec<u8>,
-    pc: usize,
+    pc: i32,
+}
+
+enum Instruction {
+    Halt,
+    Out(i32),
+    BranchIfPlus(i32, i32),
 }
 
 impl Machine {
@@ -23,31 +29,44 @@ impl Machine {
         Ok(Machine { mem, pc: 0 })
     }
 
-    fn fetch_pointer(&mut self) -> usize {
-        let bytes = &self.mem[self.pc..self.pc + 4];
+    fn fetch_pointer(&mut self) -> i32 {
+        let res = LittleEndian::read_i32(&self.mem[(self.pc as usize)..]);
         self.pc += 4;
-        LittleEndian::read_i32(bytes) as usize
+        res
+    }
+
+    fn next_instruction(&mut self) -> Result<Instruction, String> {
+        let inst = self.mem[self.pc as usize];
+        self.pc += 1;
+
+        use Instruction::*;
+        match inst {
+            0 => Ok(Halt),
+            1 => Ok(Out(self.fetch_pointer())),
+            2 => Ok(BranchIfPlus(self.fetch_pointer(), self.fetch_pointer())),
+            _ => Err(format!("Unknown instruction {}", inst)),
+        }
     }
 
     fn run(&mut self) -> Result<(), Box<Error>> {
         loop {
-            let inst = self.mem[self.pc];
-            self.pc += 1;
+            // let inst = self.mem[self.pc];
+            let inst = self.next_instruction()?;
 
+            use Instruction::*;
             match inst {
-                0 => {
-                    // HALT
+                Halt => {
                     std::process::exit(0);
                 }
-
-                1 => {
-                    let ptr = self.fetch_pointer();
-                    let ch = self.mem[ptr] as char;
+                Out(ptr) => {
+                    let ch = self.mem[ptr as usize] as char;
                     print!("{}", ch);
                 }
-                _ => {
-                    eprintln!("Unknown instruction {}", inst);
-                    std::process::exit(1);
+                BranchIfPlus(jmpptr, srcptr) => {
+                    // Branch If Plus
+                    if self.mem[srcptr as usize] < 128 {
+                        self.pc = jmpptr;
+                    }
                 }
             }
         }
