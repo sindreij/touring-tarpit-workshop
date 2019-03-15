757f9081eb72d84462e00fafaaf6000f94f35500
Sindre Johansen <sindre@iterate.no>
2019-03-15 09:53:33 +0100

    Milestone 2, hello world

--- src/main.rs
+++ src/main.rs
@@ -2,35 +2,65 @@ use std::env;
 use std::error::Error;
 use std::fs::File;
 use std::io::prelude::*;
+use std::io::Cursor;
 
-fn read_mem() -> Result<Vec<u8>, Box<Error>> {
-    let args: Vec<_> = env::args().collect();
-    let programfile = &args[1];
+use byteorder::{BigEndian, LittleEndian, ReadBytesExt, WriteBytesExt};
 
-    let mut memfile = File::open(programfile)?;
-
-    let mut mem = Vec::new();
-    memfile.read_to_end(&mut mem)?;
-
-    Ok(mem)
+struct Machine {
+    mem: Vec<u8>,
+    pc: usize,
 }
 
-fn main() -> Result<(), Box<Error>> {
-    let mem = read_mem()?;
-    let mut pc = 0;
+impl Machine {
+    fn from_args() -> Result<Machine, Box<Error>> {
+        let args: Vec<_> = env::args().collect();
+        let programfile = &args[1];
 
-    loop {
-        let inst = mem[pc];
-        pc = pc + 1;
-        match inst {
-            0 => {
-                // HALT
-                std::process::exit(0);
-            }
-            _ => {
-                eprintln!("Unknown instruction {}", inst);
-                std::process::exit(1);
+        let mut memfile = File::open(programfile)?;
+
+        let mut mem = Vec::new();
+        memfile.read_to_end(&mut mem)?;
+
+        Ok(Machine { mem, pc: 0 })
+    }
+
+    fn fetch_pointer(&mut self) -> Result<usize, Box<Error>> {
+        let bytes = &self.mem[self.pc..self.pc + 4];
+        self.pc += 4;
+
+        let mut rdr = Cursor::new(bytes);
+
+        Ok(rdr.read_i32::<LittleEndian>()? as usize)
+    }
+
+    fn run(&mut self) -> Result<(), Box<Error>> {
+        loop {
+            let inst = self.mem[self.pc];
+            self.pc += 1;
+
+            match inst {
+                0 => {
+                    // HALT
+                    std::process::exit(0);
+                }
+
+                1 => {
+                    let ptr = self.fetch_pointer()?;
+                    let ch = self.mem[ptr] as char;
+                    print!("{}", ch);
+                }
+                _ => {
+                    eprintln!("Unknown instruction {}", inst);
+                    std::process::exit(1);
+                }
             }
         }
     }
+}
+
+fn main() -> Result<(), Box<Error>> {
+    let mut machine = Machine::from_args()?;
+    machine.run()?;
+
+    Ok(())
 }
